0dedd3ebbcca91bffd24f28e83a36ac38d354313
Alberttorm9 <ratita848@gmail.com>
2023-11-28 01:09:25 +0100

    New Update

--- PLC Programs/ControlMultipleLOGO.py
+++ PLC Programs/ControlMultipleLOGO.py
@@ -1,4 +1,5 @@
 import snap7
+import string
 import time
 
 #V10.0 = Lector De Tarjetas ---------- V12.0 = Luz RGB verde
@@ -10,79 +11,93 @@ import time
 #V10.6 = Forzar Alimentación (Negado) ---------- V12.6 = Alimentación
 # ---------- V12.7 = Fallo alimentacion garaje
 
+pulsador_portal = "V10.1"
+portal_cerrado = "V12.2"
+portal_abierto = "V12.3"
+
 cantidad_de_habitaciones = 40
-numero_abiertos_cerrados = 0
 numero_fallidos = 0
 plc_a_leer = 0
+portales_ya_cerrados = []
+portales_ya_abiertos = []
+portales_ni_cerrados_ni_abiertos = []
+portales_fallidos = []
 
 # Solicitar al usuario que ingrese un número
-accion = int(input("Que quiere hacer?\nAbirir Portales - 1\nCerrar Portales - 2\nApagar Variables - 3\nLeer Variables - 4\n"))
+accion = int(input("\nQue quiere hacer?\nAbirir Portales - 1\nCerrar Portales - 2\nApagar Variables - 3\nLeer Variables - 4\n"))
 if accion == 1:
-    accion = int(input("Seguro?\nSi\nNo"))
+    accion = input("\nSeguro?\nSi\nNo\n\n")
+    inicio_tiempo = time.time()
     if accion==("Si" or "si" or "SI"):
-        inicio_tiempo = time.time()
-        for i in range(10):
+        print("\n")
+        for i in range(cantidad_de_habitaciones):
             plc = snap7.logo.Logo()
             try:
                 plc.connect(f'192.168.30.{i+101}', 0, 1)
-                if not (plc.read("V10.3")):
-                    plc.write("V10.1", 1)
+                if plc.read(f'{portal_cerrado}'):
+                    plc.write(f'{pulsador_portal}', 1)
                     time.sleep(0.5)
-                    plc.write("V10.1", 0)
+                    plc.write(f'{pulsador_portal}', 0)
                     print(f"PLC {i+101} Abriendo")
-                    numero_abiertos_cerrados += 1
+                elif plc.read(f'{portal_abierto}'):
+                    portales_ya_abiertos.append(i+101)
                 else:
-                    print(f"PLC{i+101} Ya abierto")
-                    numero_abiertos_cerrados += 1
+                    portales_ni_cerrados_ni_abiertos.append(i+101)
                 plc.disconnect()
             except Exception as err:
-                print(f"Error de conexión al PLC {i+101}: {err}")
                 print(f"PLC {i+101} Error")
-                numero_fallidos += 1
+                portales_fallidos.append(i+101)
 
-    print(f"Fallaron: {numero_fallidos}")
-    print(f"Ya abiertos: {numero_abiertos_cerrados}")
+    print(f"\n\nFallaron: {len(portales_fallidos)}")
+    print(f'Los fallidos son: {portales_fallidos}')
+    print(f"Ya abiertos: {len(portales_ya_abiertos)}")
+    print(f'Los ya abiertos son: {portales_ya_abiertos}')
+    print(f"Portales ni abiertos ni cerrados: {len(portales_ni_cerrados_ni_abiertos)}")
+    print(f'Los ni abiertos ni cerrados son: {portales_ni_cerrados_ni_abiertos}')
     fin_tiempo = time.time()
     tiempo_ejecucion = fin_tiempo - inicio_tiempo
-    print(f"El tiempo de ejecución fue de {tiempo_ejecucion} segundos.")
+    print(f"\nEl tiempo de ejecución fue de {tiempo_ejecucion} segundos.")
 
 elif accion == 2:
-    accion = int(input("Seguro?\nSi\nNo"))
+    accion = input("\nSeguro?\nSi\nNo\n\n")
+    inicio_tiempo = time.time()
     if accion==("Si" or "si" or "SI"):
-        inicio_tiempo = time.time()
-        for i in range(10):
+        print("\n")
+        for i in range(cantidad_de_habitaciones):
             plc = snap7.logo.Logo()
             try:
                 plc.connect(f'192.168.30.{i+101}', 0, 1)
-                if not (plc.read("V12.2")):
-                    plc.write("V10.1", 1)
+                if plc.read(f'{portal_abierto}'):
+                    plc.write(f'{pulsador_portal}', 1)
                     time.sleep(0.5)
-                    plc.write("V10.1", 0)
-                    print(f"PLC {i+101} Conectado")
-                    numero_abiertos_cerrados += 1
+                    plc.write(f'{pulsador_portal}', 0)
+                    print(f"PLC {i+101} Cerrando")
+                elif plc.read(f'{portal_cerrado}'):
+                    portales_ya_cerrados.append(i+101)
                 else:
-                    print(f"PLC{i+101} Ya abierto")
-                    numero_abiertos_cerrados += 1
+                    portales_ni_cerrados_ni_abiertos.append(i+101)
                 plc.disconnect()
             except Exception as err:
-                print(f"Error de conexión al PLC {i+101}: {err}")
                 print(f"PLC {i+101} Error")
-                numero_fallidos += 1
+                portales_fallidos.append(i+101)
 
-        print(f"Fallaron: {numero_fallidos}")
-        print(f"Ya abiertos: {numero_abiertos_cerrados}")
-        fin_tiempo = time.time()
-        tiempo_ejecucion = fin_tiempo - inicio_tiempo
-        print(f"El tiempo de ejecución fue de {tiempo_ejecucion} segundos.")
-        time.sleep(3)
+    print(f"\n\nFallaron: {len(portales_fallidos)}")
+    print(f'Los fallidos son: {portales_fallidos}')
+    print(f"Ya cerrados: {len(portales_ya_cerrados)}")
+    print(f'Los ya cerrados son: {portales_ya_cerrados}')
+    print(f"Portales ni abiertos ni cerrados: {len(portales_ni_cerrados_ni_abiertos)}")
+    print(f'Los ni abiertos ni cerrados son: {portales_ni_cerrados_ni_abiertos}')
+    fin_tiempo = time.time()
+    tiempo_ejecucion = fin_tiempo - inicio_tiempo
+    print(f"\nEl tiempo de ejecución fue de {tiempo_ejecucion} segundos.")
+
 elif accion == 3:
     inicio_tiempo = time.time()
     for i in range(10):
         plc = snap7.logo.Logo()
         try:
             plc.connect(f'192.168.30.{i+101}', 0, 1)
-            for j in range(7):
-                plc.write(f'10.{j}', 0)
+            plc.write("V10", 0)
             plc.disconnect()
             print(f'Variables del PLC {i+101} desactivadas')
         except Exception:
@@ -95,7 +110,6 @@ elif accion == 3:
     fin_tiempo = time.time()
     tiempo_ejecucion = fin_tiempo - inicio_tiempo
     print(f"El tiempo de ejecución fue de {tiempo_ejecucion} segundos.")
-    time.sleep(3)
 
 elif accion == 4:
     while (plc_a_leer < 1) or (plc_a_leer > cantidad_de_habitaciones):
