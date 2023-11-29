b7c4535f10f476417fe3941867505db4f5e8fa35
Alberttorm9 <ratita848@gmail.com>
2023-11-29 12:20:38 +0100

    New Update

--- PLC Programs/Esbribir_Bit.py
+++ PLC Programs/Esbribir_Bit.py
@@ -1,10 +1,33 @@
 import snap7
 import time
-ip = "192.168.30.104"
+import Tools as ts
+act = 3
 plc = snap7.logo.Logo()
-plc.connect(ip, 0, 1)
-plc.write("V10.1",1)
-time.sleep(0.5)
-plc.write("V10.1",0)
 
-plc.disconnect()
+for i in range(4):
+    ip = f'192.168.30.{i+101}'
+    if act == 3:
+        act = int(input("Open or close all?\nYes - 2\nGo step x step - 1\nNo - 0\n\n"))
+    if act == 1:
+        plc = ts.try_to_connect(ip, 0, 1)
+        if plc.read("V12.2"):
+            si_no = input(f'\nPortal {ip} Cerrado, Desea Abrir?\n\nSi - 1\nNo - 0\n')
+            if si_no:
+                plc.write("V10.1",1)
+                time.sleep(0.5)
+                plc.write("V10.1",0)
+        elif plc.read("V12.3"):
+            si_no = input(f'\nPortal {ip} Abierto, Desea Cerrar?\nSi - 1\nNo - 0\n')
+            if si_no:
+                plc.write("V10.1",1)
+                time.sleep(0.5)
+                plc.write("V10.1",0)
+        else:
+            print(f'Error en plc {ip}')
+        plc.disconnect()
+    elif act == 2:
+        ts.try_to_connect(ip, 0, 1)
+        plc.write("V10.1",1)
+        time.sleep(0.5)
+        plc.write("V10.1",0)
+        plc.disconnect()
---
+++ PLC Programs/Tools.py
@@ -0,0 +1,14 @@
+import snap7
+
+def try_to_connect(ip, rack, slot):
+    plc = snap7.logo.Logo()
+    try:
+     plc.connect(ip, rack, slot)
+     return(plc)    
+    except Exception:
+        try:
+          plc.connect(ip, rack, slot)
+          return (plc)
+        except Exception:
+            return ("Error")
+try_to_connect("192.168.30.101", 0, 1)
