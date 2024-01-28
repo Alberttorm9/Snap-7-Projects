e3873fe0672408ff6c7e74ed03ad9bc7146a7bd7
Albertto_rm <ratita848@gmail.com>
2024-01-28 13:46:43 +0100

    New Commit

--- DB/Export.py
+++ DB/Export.py
@@ -1,12 +1,12 @@
 import tkinter as tk
-from tkinter import ttk, PhotoImage, Tk, Frame, Label
+from tkinter import PhotoImage, ttk 
 from PIL import Image, ImageTk
 import pyodbc
 import os
 import re
 import sys
 import configparser
-from datetime import datetime
+from datetime import datetime, date, timedelta
 import openpyxl
 from openpyxl.styles import Font
 
@@ -19,8 +19,8 @@ ScreenHeight = int(config["GEOMETRIA"]["ALTO"])
 ScreenWidth = int(config["GEOMETRIA"]["ANCHO"])
 
 # Conexión a la base de datos
-server = 'LAPTOP-3UQV2BFJ\\SQLEXPRESS' 
-database = 'Motel_Panamá' 
+server = str(config["DB"]["DBSERVER"]) 
+database = str(config["DB"]["DBNAME"]) 
 conn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';Trusted_Connection=yes')
 
 def export_Info(tabla, desde, hasta, type):
@@ -72,14 +72,57 @@ def exportar_excel(rows, encabezados,CantidadValores, carpeta_exports, tabla):
 
     wb.save(f'{carpeta_exports}\{tabla} {datetime.now().strftime("%d-%m-%y")}.xlsx')
 
-def set_actual_date():
+def set_actual_date(type):
     ActualDate= datetime.now().strftime('%Y-%m-%d')
-    ToEntryReports.delete(0, 'end')
-    ToEntryReports.insert(0, ActualDate)
-    ToEntryExits.delete(0, 'end')
-    ToEntryExits.insert(0, ActualDate)
-    ToEntryHabs.delete(0, 'end')
-    ToEntryHabs.insert(0, ActualDate)
+    if type=="Reports":
+        ToEntryReports.delete(0, 'end')
+        ToEntryReports.insert(0, ActualDate)
+    elif type=="Exits":
+        ToEntryExits.delete(0, 'end')
+        ToEntryExits.insert(0, ActualDate)
+    elif type=="Habs":
+        ToEntryHabs.delete(0, 'end')
+        ToEntryHabs.insert(0, ActualDate)
+
+def export_month(type):
+    ActualDate= datetime.now().strftime('%Y-%m-%d')
+    first_day_of_month = date.today().replace(day=1)
+    if type=="Reports":
+        FromEntryReports.delete(0, 'end')
+        FromEntryReports.insert(0, first_day_of_month)
+        ToEntryReports.delete(0, 'end')
+        ToEntryReports.insert(0, ActualDate)
+    elif type=="Exits":
+        FromEntryExits.delete(0, 'end')
+        FromEntryExits.insert(0, first_day_of_month)
+        ToEntryExits.delete(0, 'end')
+        ToEntryExits.insert(0, ActualDate)
+    elif type=="Habs":
+        FromEntryHabs.delete(0, 'end')
+        FromEntryHabs.insert(0, first_day_of_month)
+        ToEntryHabs.delete(0, 'end')
+        ToEntryHabs.insert(0, ActualDate)
+
+def export_day(type):
+    ActualDate= datetime.now().strftime('%Y-%m-%d')
+    yesterday = datetime.now() - timedelta(days=1)
+    yesterday = yesterday.strftime('%Y-%m-%d')
+    if type=="Reports":
+        FromEntryReports.delete(0, 'end')
+        FromEntryReports.insert(0, yesterday)
+        ToEntryReports.delete(0, 'end')
+        ToEntryReports.insert(0, ActualDate)
+    elif type=="Exits":
+        FromEntryExits.delete(0, 'end')
+        FromEntryExits.insert(0, yesterday)
+        ToEntryExits.delete(0, 'end')
+        ToEntryExits.insert(0, ActualDate)
+    elif type=="Habs":
+        FromEntryHabs.delete(0, 'end')
+        FromEntryHabs.insert(0, yesterday)
+        ToEntryHabs.delete(0, 'end')
+        ToEntryHabs.insert(0, ActualDate)
+        
 
 def export(type):
     if type=="Reports":
@@ -124,21 +167,22 @@ def show_Habs():
 # Interfaz de usuario
 root = tk.Tk()
 root.title("Exportar Información")
-imagen_fondo = Image.open("fondo.png")
-imagen_fondo = ImageTk.PhotoImage(imagen_fondo)
-etiqueta_fondo = Label(root, image=imagen_fondo)
-etiqueta_fondo.place(x=0, y=0, relwidth=1, relheight=1)
+root.config(bg='#505050')
 
-#TkStyle
-Tkstyle = ttk.Style()
-Tkstyle.configure('Custom.TButton', font=('Helvetica', 12), padding=10)
-Tkstyle.configure('Title.TLabel', font=('Arial', 14, 'bold'))
-Tkstyle.configure('Invisible.TButton', borderwidth=0, borderheight=0, highlightthickness=0, relief="flat")
+# Configure the style for the buttons
+style = ttk.Style()
+style.configure('TButton', font=('calibri', 10, 'bold'), foreground='black')
+style.configure('TLabel', font=('calibri', 10), foreground='black')
+style.configure('TEntry', font=('calibri', 10), foreground='black')
+
+text_font = ('Helvetica', 12, 'bold')
+title_font = ('Helvetica', 20, 'bold')
+
 #Frames
-FrameExportaciones = tk.Frame(root, width=int(ScreenWidth/2), height=int(ScreenHeight/2))
-FrameExportReports = tk.Frame(root)
-FrameExportExit = tk.Frame(root)
-FrameExportHabs = tk.Frame(root)
+FrameExportaciones = tk.Frame(root, width=int(ScreenWidth/2), height=int(ScreenHeight/2), bg= '#505050')
+FrameExportReports = tk.Frame(root, bg= '#505050')
+FrameExportExit = tk.Frame(root, bg= '#505050')
+FrameExportHabs = tk.Frame(root, bg= '#505050')
 
 
 
@@ -150,12 +194,12 @@ root.attributes("-topmost", True)
 
 #Close App
 x_photo = PhotoImage(file=os.path.abspath("x_button.png"))
-SysCloseButton = tk.Button(root, image=x_photo, borderwidth=0, highlightthickness=0,  command=sys.exit)
+SysCloseButton = tk.Button(root, image=x_photo, borderwidth=0, bg= '#505050', command=sys.exit)
 SysCloseButton.place(relx=1, x=0, y=0, anchor='ne')
 
 #Go Back
 back_phooto = PhotoImage(file=os.path.abspath("Back_Button.png"))
-GoBackButton = ttk.Button(root, image=back_phooto, style='Invisible.TButton',command=GoBack)
+GoBackButton = tk.Button(root, image=back_phooto, borderwidth=0, bg= '#505050', command=GoBack)
 
 #Initial Frame
 FrameExportaciones.place(relx=0.5, rely=0.5, anchor='center')
@@ -163,60 +207,73 @@ FrameExportaciones.place(relx=0.5, rely=0.5, anchor='center')
 #####################################################################################################################################################
 
 #Open Reports
-OpenFrameReportes = ttk.Button(FrameExportaciones, text="Exportar Reportes", command=show_Reports, style='Custom.TButton')
+OpenFrameReportes = ttk.Button(FrameExportaciones, text="Exportar Reportes", command=show_Reports)
 OpenFrameReportes.grid(row=1, column=0, padx=10, pady=10)
 
 #To Frame Reports
-LabelReports = ttk.Label(FrameExportReports, text="Exportador Tabla Reportes", style='Title.TLabel')
+LabelReports = tk.Label(FrameExportReports, text="Exportador Tabla Reportes", bg= '#505050', font=title_font, fg='white')
 LabelReports.grid(row=0, columnspan=3, pady=5)
 
-FromLabelReports = ttk.Label(FrameExportReports, text="Desde:")
+FromLabelReports = tk.Label(FrameExportReports, text="Desde:", bg= '#505050', font=text_font, fg='white')
 FromLabelReports.grid(row=1, column=0)
-FromEntryReports = ttk.Entry(FrameExportReports)
+FromEntryReports = tk.Entry(FrameExportReports, width=12, font= 10)
 FromEntryReports.grid(row=1, column=1)
 
-ToLabelReports = ttk.Label(FrameExportReports, text="Hasta:")
+ToLabelReports = tk.Label(FrameExportReports, text="Hasta:", bg='#505050', font=text_font, fg='white')
 ToLabelReports.grid(row=2, column=0)
-ToEntryReports = ttk.Entry(FrameExportReports)
+ToEntryReports = tk.Entry(FrameExportReports, width=12, font= 10)
 ToEntryReports.grid(row=2, column=1)
 
-ActualDateButtonReports = ttk.Button(FrameExportReports, text="Fecha actual", command=set_actual_date)
+ActualDateButtonReports = ttk.Button(FrameExportReports, text="Fecha actual", command=lambda:set_actual_date(str("Reports")))
 ActualDateButtonReports.grid(row=2, column=2)
 
+#Exportar un día
+ExportLastDayReportes = ttk.Button(FrameExportReports, text="Exportar Último Dia", width=17, command=lambda:export_day(str("Reports")))
+ExportLastDayReportes.grid(row=1, column=4, padx=5, pady=2, sticky="w")
+
+ExportLastMonthReportes = ttk.Button(FrameExportReports, text="Exportar Último Mes", width=17, command=lambda:export_month(str("Reports")))
+ExportLastMonthReportes.grid(row=2, column=4, padx=5, pady=2, sticky="w")
+
 ExportButtonReports = ttk.Button(FrameExportReports, text="Exportar", command=lambda:export(str("Reports")))
-ExportButtonReports.grid(row=3, columnspan=2)
+ExportButtonReports.grid(row=3, column=1, columnspan=1, pady=(10, 0), sticky="nsew")
 
 #####################################################################################################################################################
 
-#Open System Exits
-OpenFrameExit = ttk.Button(FrameExportaciones, text="Exportar Salidas Del Sistema", command=show_Exit, style='Custom.TButton')
+# Update the widget styles
+OpenFrameExit = ttk.Button(FrameExportaciones, text="Exportar Salidas Del Sistema", command=show_Exit)
 OpenFrameExit.grid(row=1, column=1, padx=10, pady=10)
 
 #To Frame Exits
-FromLabelExits = ttk.Label(FrameExportExit, text="Desde:")
-FromLabelExits.grid(row=1, column=1)
-FromEntryExits = ttk.Entry(FrameExportExit)
-FromEntryExits.grid(row=1, column=2)
+LabelExits = tk.Label(FrameExportExit, text="Exportador Tabla Reportes", bg= '#505050', font=title_font, fg='white')
+LabelExits.grid(row=0, columnspan=3, pady=5)
 
-ToLabelExits = ttk.Label(FrameExportExit, text="Hasta:")
-ToLabelExits.grid(row=2, column=1)
-ToEntryExits = ttk.Entry(FrameExportExit)
-ToEntryExits.grid(row=2, column=2)
+FromLabelExits = tk.Label(FrameExportExit, text="Desde:", bg= '#505050', font=text_font, fg='white')
+FromLabelExits.grid(row=1, column=0)
+FromEntryExits = tk.Entry(FrameExportExit, width=12, font= 10)
+FromEntryExits.grid(row=1, column=1)
+
+ToLabelExits = tk.Label(FrameExportExit, text="Hasta:", bg='#505050', font=text_font, fg='white')
+ToLabelExits.grid(row=2, column=0)
+ToEntryExits = tk.Entry(FrameExportExit, width=12, font= 10)
+ToEntryExits.grid(row=2, column=1)
 
 ActualDateButtonExits = ttk.Button(FrameExportExit, text="Fecha actual", command=set_actual_date)
-ActualDateButtonExits.grid(row=2, column=3)
+ActualDateButtonExits.grid(row=2, column=2)
 
 ExportButtonExits = ttk.Button(FrameExportExit, text="Exportar", command=lambda:export(str("Exits")))
-ExportButtonExits.grid(row=3, columnspan=3)
+ExportButtonExits.grid(row=3, column=1, columnspan=1, pady=(10, 0), sticky="nsew")
 
 #####################################################################################################################################################
 
 #Open Habs
-OpenFrameHabs = ttk.Button(FrameExportaciones, text="Exportar Limpieza Habitaciones", command=show_Habs, style='Custom.TButton')
+OpenFrameHabs = ttk.Button(FrameExportaciones, text="Exportar Limpieza Habitaciones", command=show_Habs)
 OpenFrameHabs.grid(row=1, column=2, padx=10, pady=10)
 
 #To Frame Habs
-TableHabs = ttk.Label(FrameExportHabs, text="Tabla:")
+LabelHabs = tk.Label(FrameExportHabs, text="Exportador Tabla Habitaciones", bg= '#505050', font=title_font, fg='white')
+LabelHabs.grid(row=0, columnspan=6, pady=5)
+
+TableHabs = tk.Label(FrameExportHabs, text="Tabla:", bg= '#505050', font=text_font, fg='white')
 TableHabs.grid(row=1, column=1)
 with open('archivo_valores.txt', 'r') as file:
     ListHabs = file.readlines()
@@ -224,21 +281,21 @@ with open('archivo_valores.txt', 'r') as file:
 TableComboxHabs = ttk.Combobox(FrameExportHabs, values=ListHabs, width=31)
 TableComboxHabs.grid(row=1, column=2, padx=2)
 
-FromLabelHabs = ttk.Label(FrameExportHabs, text="Desde:")
+FromLabelHabs = tk.Label(FrameExportHabs, text="Desde:", bg= '#505050', font=text_font, fg='white')
 FromLabelHabs.grid(row=1, column=3)
-FromEntryHabs = ttk.Entry(FrameExportHabs)
+FromEntryHabs = tk.Entry(FrameExportHabs)
 FromEntryHabs.grid(row=1, column=4)
 
-ToLabelHabs = ttk.Label(FrameExportHabs, text="Hasta:")
+ToLabelHabs = tk.Label(FrameExportHabs, text="Hasta:", bg= '#505050', font=text_font, fg='white')
 ToLabelHabs.grid(row=2, column=3)
-ToEntryHabs = ttk.Entry(FrameExportHabs)
+ToEntryHabs = tk.Entry(FrameExportHabs)
 ToEntryHabs.grid(row=2, column=4)
 
 ActualDateButtonHabs = ttk.Button(FrameExportHabs, text="Fecha actual", command=set_actual_date)
-ActualDateButtonHabs.grid(row=2, column=5)
+ActualDateButtonHabs.grid(row=2, column=5, padx=10)
 
 ExportButtonHabs = ttk.Button(FrameExportHabs, text="Exportar", command=lambda:export(str("Habs")))
-ExportButtonHabs.grid(row=4, columnspan=6)
+ExportButtonHabs.grid(row=4, columnspan=6, pady=10)
 
 #####################################################################################################################################################
 
